689af893f946ed7a7f2a4629e14236ccf693b064
radavilae <raul.davila.esp@gmail.com>
2026-01-31 23:50:29 +0100

    responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '../contexts/LanguageContext';
 import LanguageSelector from './LanguageSelector';
 
 const Navigation = () => {
   const { t } = useLanguage();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <nav id="navbar">
-      <ul id="menu-options">
-        <li><Link to="/">{t('nav.home')}</Link></li>
-        <li><Link to="/bio">{t('nav.bio')}</Link></li>
-        <li><Link to="/research">{t('nav.research')}</Link></li>
-        <li><Link to="/projects">{t('nav.projects')}</Link></li>
-        <li><Link to="/contact">{t('nav.contact')}</Link></li>
+      {/* Hamburger Menu Button */}
+      <button 
+        className="hamburger-menu" 
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <span className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></span>
+        <span className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></span>
+        <span className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></span>
+      </button>
+
+      {/* Menu Items */}
+      <ul id="menu-options" className={isMenuOpen ? 'mobile-open' : ''}>
+        <li><Link to="/" onClick={closeMenu}>{t('nav.home')}</Link></li>
+        <li><Link to="/bio" onClick={closeMenu}>{t('nav.bio')}</Link></li>
+        <li><Link to="/research" onClick={closeMenu}>{t('nav.research')}</Link></li>
+        <li><Link to="/projects" onClick={closeMenu}>{t('nav.projects')}</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>{t('nav.contact')}</Link></li>
         <LanguageSelector />
       </ul>
     </nav>
